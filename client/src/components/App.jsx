9d9ddfecfa67bfc98baac7cf1e844080d0a0652c
George <georgezqian@gmail.com>
2018-12-21 11:44:37 -0800

    Successfully access watch id from client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const App = props => (
-  <h1>HELLO</h1>
-);
+const App = props => {
+  const url = window.location.href;
+  const urlSections = url.split('/');
+  const wid = urlSections[urlSections.length - 2];
+  const displayString = `Watch id on current page is: ${wid}`;
+  return <h1>{displayString}</h1>;
+};
 
 export default App;
