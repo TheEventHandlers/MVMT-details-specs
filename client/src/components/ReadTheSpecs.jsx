6ae8043f9dd101b88d6dd158afa8c7c466b62a0b
George <georgezqian@gmail.com>
2018-12-24 10:06:14 -0500

    Change descriptors to be bold and small caps

--- client/src/components/ReadTheSpecs.jsx
+++ client/src/components/ReadTheSpecs.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 const ReadTheSpecs = props => {
   return (
     <div>
-      <h5>READ THE SPECS</h5>
-      <div>case size: {props.specs.case_size}</div>
-      <div>case thickness: {props.specs.case_thickness}</div>
-      <div>strap width: {props.specs.strap_width}</div>
-      <div>movement: {props.specs.movement}</div>
-      <div>glass: {props.specs.glass}</div>
-      <div>water resistance: {props.specs.water_resistance}</div>
+      <div>
+        <span className="details-category">case size </span>
+        <span className="details-info">{props.specs.case_size}</span>
+      </div>
+      <div>
+        <span className="details-category">case thickness </span>
+        <span className="details-info">{props.specs.case_thickness}</span>
+      </div>
+      <div>
+        <span className="details-category">strap width </span>
+        <span className="details-info">{props.specs.strap_width}</span>
+      </div>
+      <div>
+        <span className="details-category">movement </span>
+        <span className="details-info">{props.specs.movement}</span>
+      </div>
+      <div>
+        <span className="details-category">glass </span>
+        <span className="details-info">{props.specs.glass}</span>
+      </div>
+      <div>
+        <span className="details-category">water resistance </span>
+        <span className="details-info">{props.specs.water_resistance}</span>
+      </div>
     </div>
   );
 };
--- client/src/components/SeeTheStyle.jsx
+++ client/src/components/SeeTheStyle.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 const SeeTheStyle = props => {
   return (
     <div>
-      <h5>SEE THE STYLE</h5>
-      <div>case description: {props.specs.case_description}</div>
-      <div>dial: {props.specs.dial}</div>
-      <div>dial details: {props.specs.dial_details}</div>
-      <div>strap: {props.specs.strap}</div>
-      <div>interchangeable strap: {props.specs.interchangeable_strap}</div>
-      <div>subdials: {props.specs.subdials}</div>
+      <div>
+        <span className="details-category">case description </span>
+        <span className="details-info">{props.specs.case_description}</span>
+      </div>
+      <div>
+        <span className="details-category">dial </span>
+        <span className="details-info">{props.specs.dial}</span>
+      </div>
+      <div>
+        <span className="details-category">dial details </span>
+        <span className="details-info">{props.specs.dial_details}</span>
+      </div>
+      <div>
+        <span className="details-category">strap </span>
+        <span className="details-info">{props.specs.strap}</span>
+      </div>
+      <div>
+        <span className="details-category">interchangeable strap </span>
+        <span className="details-info">{props.specs.interchangeable_strap}</span>
+      </div>
+      <div>
+        <span className="details-category">subdials </span>
+        <span className="details-info">{props.specs.subdials}</span>
+      </div>
     </div>
   );
 };
