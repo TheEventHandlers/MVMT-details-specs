10ff42e96dd97f2fc78acd18411398f97431509f
George <georgezqian@gmail.com>
2018-12-28 15:23:01 -0500

    Complete see-the-fit modal

--- client/src/components/SeeTheFit.jsx
+++ client/src/components/SeeTheFit.jsx
@@ -18,15 +18,9 @@ const SeeTheFit = ({ show, handleClose, specs }) => {
   return (
     <div className={modalClassName}>
       <section className="details-modal-main">
-        <img className="details-img"
-             id="detials-fit-photo"
-             src={specs.fit_photo}/>
-        <div className="details-modal-watch-size">
-          {watchSize}
-        </div>
-        <div className="details-modal-title">
-          {specs.series} series size guide
-        </div>
+        <img className="details-img" id="detials-fit-photo" src={specs.fit_photo}/>
+        <div className="details-modal-watch-size"> {watchSize} </div>
+        <div className="details-modal-title"> {specs.series} series size guide </div>
         <div className="details-modal-description">
           the {specs.series} series watches feature a stylish {specs.case_size}mm diameter case size.
           case depth is approximiatey {specs.case_thickness}mm. this watch style is a modern classic...
@@ -34,24 +28,28 @@ const SeeTheFit = ({ show, handleClose, specs }) => {
         </div> 
         <div className="details-small-specs">
           <div>
-            <span className="details-small-spec-descriptor">
-              case:
-            </span>
-            <span className="details-small-spec-value">
-              {specs.case_size} mm
-            </span>
+            <span className="details-small-spec-descriptor"> case: </span>
+            <span className="details-small-spec-value"> {specs.case_size} mm </span>
           </div>
           <div>
-            <span className="details-small-spec-descriptor">
-              thickness:
-            </span>
-            <span className="details-small-spec-value">
-              {specs.case_thickness} mm
-            </span>
+            <span className="details-small-spec-descriptor"> thickness: </span>
+            <span className="details-small-spec-value"> {specs.case_thickness} mm </span>
           </div>
         </div>
-        <div className="details-footnote">
-          *model wrist 7.5in circumference
+        <div className="details-footnote"> *model wrist 7.5in circumference </div>
+        <div className="details-slider-container" id="details-thickness-slider-container">
+          CASE THICKNESS:
+          <div className="details-slider"></div>
+          <div className="details-slider-fill"></div>
+          <span className="details-slider-left">flat</span>
+          <span className="details-slider-right">tall</span>
+        </div>
+        <div className="details-slider-container" id="details-size-slider-container">
+          CASE SIZE:
+          <div className="details-slider"></div>
+          <div className="details-slider-fill"></div>
+          <span className="details-slider-left">small</span>
+          <span className="details-slider-right">large</span>
         </div>
         <button id="details-close-modal" onClick={handleClose}> x </button>
       </section>
