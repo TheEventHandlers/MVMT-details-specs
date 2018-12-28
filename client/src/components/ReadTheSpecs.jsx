7478a3636aaa0a0d3411bd66f10d04728b840a7c
George <georgezqian@gmail.com>
2018-12-28 11:51:56 -0500

    Refactor spec components to use object destructuring

--- client/src/components/ReadTheSpecs.jsx
+++ client/src/components/ReadTheSpecs.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
-const ReadTheSpecs = props => {
+const ReadTheSpecs = ({ specs }) => {
   return (
     <div className="details-table">
       <div className="details-table-row">
         <span className="details-category">case size </span>
-        <span className="details-info">{props.specs.case_size} mm</span>
+        <span className="details-info">{specs.case_size} mm</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">case thickness </span>
-        <span className="details-info">{props.specs.case_thickness} mm</span>
+        <span className="details-info">{specs.case_thickness} mm</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">strap width </span>
-        <span className="details-info">{props.specs.strap_width} mm</span>
+        <span className="details-info">{specs.strap_width} mm</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">movement </span>
-        <span className="details-info">{props.specs.movement}</span>
+        <span className="details-info">{specs.movement}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">glass </span>
-        <span className="details-info">{props.specs.glass}</span>
+        <span className="details-info">{specs.glass}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">water resistance </span>
-        <span className="details-info">{props.specs.water_resistance} ATM</span>
+        <span className="details-info">{specs.water_resistance} ATM</span>
       </div>
     </div>
   );
--- client/src/components/SeeTheStyle.jsx
+++ client/src/components/SeeTheStyle.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
-const SeeTheStyle = props => {
+const SeeTheStyle = ({ specs }) => {
   return (
     <div className="details-table">
       <div className="details-table-row">
         <span className="details-category">case description </span>
-        <span className="details-info">{props.specs.case_description}</span>
+        <span className="details-info">{specs.case_description}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">dial </span>
-        <span className="details-info">{props.specs.dial}</span>
+        <span className="details-info">{specs.dial}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">dial details </span>
-        <span className="details-info">{props.specs.dial_details}</span>
+        <span className="details-info">{specs.dial_details}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">strap </span>
-        <span className="details-info">{props.specs.strap}</span>
+        <span className="details-info">{specs.strap}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">interchangeable strap </span>
-        <span className="details-info">{props.specs.interchangeable_strap}</span>
+        <span className="details-info">{specs.interchangeable_strap}</span>
       </div>
       <div className="details-table-row">
         <span className="details-category">subdials </span>
-        <span className="details-info">{props.specs.subdials}</span>
+        <span className="details-info">{specs.subdials}</span>
       </div>
     </div>
   );
