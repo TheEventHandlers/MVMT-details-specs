340137675b219ea9d2297889bef55abc1534caef
George <georgezqian@gmail.com>
2019-01-02 16:46:38 -0800

    Rename div id to be Details instead of app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Details from './components/App.jsx';
 
-ReactDOM.render(<Details />, document.getElementById('app'));
+ReactDOM.render(<Details />, document.getElementById('Details'));
